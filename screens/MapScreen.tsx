cd3c669deb570aea08150ca07e0e6e8a9620dc18
MoreiraSte <casagrandestephany91@gmail.com>
2023-02-24 11:25:23 -0300

    emulator google maps with marker and callout

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,12 +1,31 @@
-import { StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text } from 'react-native';
+import MapView, { Marker, Callout} from 'react-native-maps';
 
-import { Text, View } from '../components/Themed';
+import {  View } from '../components/Themed';
 
 export default function MapScreen() {
+
+  const [latitude,setLatitude] = useState(-22.913947865776727)
+  const [longitude,setLongitude] = useState(-47.068129919628475)
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Screen Map</Text>
-      
+     <MapView style={styles.map}
+      initialRegion={{
+        latitude: -22.913947865776727,
+        longitude: -47.068129919628475,
+        latitudeDelta: 0.003,
+        longitudeDelta: 0.003,
+    }}
+  >
+    <Marker coordinate={{ latitude : latitude , longitude : longitude }}>
+      <Callout>
+        <Text>SENAI</Text>
+      </Callout>
+    </Marker>
+
+  </MapView>
     </View>
   );
 }
@@ -14,16 +33,9 @@ export default function MapScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
