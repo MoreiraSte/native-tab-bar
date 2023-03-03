fe6220011138e276f5b5a06e8ad412e575c8921c
MoreiraSte <casagrandestephany91@gmail.com>
2023-03-03 10:25:58 -0300

    localização através de lista

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,31 +1,43 @@
 import { useState } from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { FlatList, StyleSheet, Text } from 'react-native';
 import MapView, { Marker, Callout} from 'react-native-maps';
 
 import {  View } from '../components/Themed';
 
 export default function MapScreen() {
 
-  const [latitude,setLatitude] = useState(-22.913947865776727)
-  const [longitude,setLongitude] = useState(-47.068129919628475)
+ 
 
+
+  const lista = [ {lat: -22.913947865776727,long: -47.068129919628475,nome:'senai'}, 
+    {lat:-22.86336876561494, long: -47.21348983091278,nome:'shopping hortolândia'},
+    {lat:-22.892263725154884,long: -47.02742494440435,nome:'shopping iguatemi'},
+     {lat:-22.847232240041055, long:-47.0628527174211,nome:'shopping dom pedro'},
+    {lat:-22.925073512554206, long:-47.12749971741982,nome:'shopping bandeiras'
+  }]
+
+  
   return (
     <View style={styles.container}>
+        
      <MapView style={styles.map}
       initialRegion={{
-        latitude: -22.913947865776727,
+        latitude:-22.913947865776727 ,
         longitude: -47.068129919628475,
         latitudeDelta: 0.003,
         longitudeDelta: 0.003,
     }}
   >
-    <Marker coordinate={{ latitude : latitude , longitude : longitude }}>
+    
+    {lista.map((item) => (
+    <Marker key={item.nome} coordinate={{ latitude : item.lat , longitude :item.long }}>
       <Callout>
-        <Text>SENAI</Text>
+        <Text>{item.nome}</Text>
       </Callout>
     </Marker>
+    ))}
 
-  </MapView>
+</MapView>
     </View>
   );
 }
